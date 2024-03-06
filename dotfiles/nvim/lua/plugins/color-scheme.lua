20b234780a45a811d2a88435733ebb221ebc1f32
Florian Sanders <florian.sanders@clever-cloud.com>
2024-03-06 08:47:01 +0100

    chore(neovim/color-scheme): remove unused color schemes

--- dotfiles/nvim/lua/plugins/color-scheme.lua
+++ dotfiles/nvim/lua/plugins/color-scheme.lua
@@ -1,42 +1,10 @@
 return {
 	{
-		{
-			"rose-pine/neovim",
-			name = "rose-pine",
-			opts = {},
-		},
-		{
-			"edeneast/nightfox.nvim",
-			opts = {
-				options = {
-					transparent = true,
-				},
-			},
-		},
-		{
-			"catppuccin/nvim",
-			name = "catppuccin",
-			opts = {
-				transparent_background = true,
-				flavour = "frappe",
-			},
-		},
-		{
-			"AlexvZyl/nordic.nvim",
-			opts = {
-				transparent_bg = true,
-				swap_backgrounds = true,
-				cursorline = {
-					bg = "#191D24",
-				},
-			},
-		},
-	},
-	{
-		"rebelot/kanagawa.nvim",
+		"edeneast/nightfox.nvim",
 		opts = {
-			transparent = true,
-			theme = "wave",
+			options = {
+				transparent = true,
+			},
 		},
 	},
 	{
