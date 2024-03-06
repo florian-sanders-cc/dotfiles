374cd9a4abe73edd6b832e7731af137e8c96cde3
Florian Sanders <florian.sanders@clever-cloud.com>
2024-03-06 17:24:55 +0100

    chore(random-labels): upgrade to 1.0.1

--- hosts/shared/programs/random-labels.nix
+++ hosts/shared/programs/random-labels.nix
@@ -1,31 +1,31 @@
-{
-  stdenv,
-  fetchFromGitHub,
-  buildNpmPackage,
-  nodejs,
+{ stdenv
+, fetchFromGitHub
+, buildNpmPackage
+, nodejs
+,
 }:
 
 stdenv.mkDerivation (finalAttrs: rec {
   pname = "random-labels";
-  version = "1.0.0";
+  version = "1.0.1";
 
   src = fetchFromGitHub {
     owner = "CleverCloud";
     repo = "random-labels";
     rev = "v${version}";
-    hash = "sha256-7MxZ2Ez7nixCG5XHwIpeKSSGEGweyLVnqefvf7SUV+g=";
+    hash = "sha256-CBB2ZS/CvCHjKgnMd+LvJ/GPcWVq3T1iRd3iLZ/G7wM=";
   };
 
   nativeBuildInputs = [ nodejs ];
 
   buildInputs = [
-     nodejs
+    nodejs
   ];
 
   randomLabelsBuilt = buildNpmPackage {
     pname = "random-labels";
     inherit version src;
-    npmDepsHash = "sha256-rtWBV1ziMuiNAyi3K/Ay5srxmjRmN2FlZ6e0uI2ZZzc=";
+    npmDepsHash = "sha256-WrtYlsf7YtbQfKYFeuR3D3C6KcS6UktbbEOIJ7gIaVQ=";
     dontNpmBuild = true;
   };
 
