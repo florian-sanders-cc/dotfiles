11bb8e9a9720f5760559a8e04dd236d85a6828f0
Florian Sanders <florian.sanders@clever-cloud.com>
2024-03-08 09:16:50 +0100

    feat(nixos/tuxedo): add tuxedo-rs and deps

--- hosts/shared/hyprland.nix
+++ hosts/shared/hyprland.nix
@@ -11,10 +11,10 @@
 
   gtk = {
     enable = true;
-    theme = {
-      package = pkgs.flat-remix-gtk;
-      name = "Flat-Remix-GTK-Grey-Darkest";
-    };
+    # theme = {
+    #   package = pkgs.flat-remix-gtk;
+    #   name = "Flat-Remix-GTK-Grey-Darkest";
+    # };
 
     iconTheme = {
       package = pkgs.gnome.adwaita-icon-theme;
@@ -27,43 +27,45 @@
     };
   };
 
-  home.packages = with pkgs; [ 
-      swaylock
-      swayosd
-      hyprpaper
-      wl-clipboard
-      cliphist
-      xfce.thunar
-      swaynotificationcenter
-      fuzzel
-      wlogout
-      pavucontrol
-      waybar
-      slurp
-      grim
-      grimblast
-      kdePackages.qtwayland
-      lxqt.lxqt-policykit
-    ];
+  home.packages = with pkgs; [
+    swaylock
+    swayosd
+    hyprpaper
+    wl-clipboard
+    cliphist
+    xfce.thunar
+    swaynotificationcenter
+    fuzzel
+    wlogout
+    pavucontrol
+    waybar
+    slurp
+    grim
+    grimblast
+    kdePackages.qtwayland
+    lxqt.lxqt-policykit
+    gtk4
+    libadwaita
+  ];
 
-    programs.keychain = {
-      enable = true;
-      keys = [ "id_ed25519" ];
-      agents = [ "ssh" "gpg 22B2EAD482730FC2" ];
-    };
+  programs.keychain = {
+    enable = true;
+    keys = [ "id_ed25519" ];
+    agents = [ "ssh" "gpg 22B2EAD482730FC2" ];
+  };
 
-    home.file.".config/waybar".source = ../../dotfiles/waybar;
-    home.file.".config/waybar/config.jsonc".source = ../../dotfiles/waybar/config.jsonc;
-    home.file.".config/waybar/modules.jsonc".source = ../../dotfiles/waybar/modules.jsonc;
-    home.file.".config/waybar/style.css".source = ../../dotfiles/waybar/style.css;
-    home.file.".config/waybar/launch.sh" = {
-        source = ../../dotfiles/waybar/launch.sh;
-        executable = true;
-    };
-    # home.file.".config/wleave".source = ../../dotfiles/wleave;
-    home.file.".config/fuzzel".source = ../../dotfiles/fuzzel;
-    home.file.".config/swaylock".source = ../../dotfiles/swaylock;
-    home.file.".config/hypr/".source = ../../dotfiles/hypr;
+  home.file.".config/waybar".source = ../../dotfiles/waybar;
+  home.file.".config/waybar/config.jsonc".source = ../../dotfiles/waybar/config.jsonc;
+  home.file.".config/waybar/modules.jsonc".source = ../../dotfiles/waybar/modules.jsonc;
+  home.file.".config/waybar/style.css".source = ../../dotfiles/waybar/style.css;
+  home.file.".config/waybar/launch.sh" = {
+    source = ../../dotfiles/waybar/launch.sh;
+    executable = true;
+  };
+  # home.file.".config/wleave".source = ../../dotfiles/wleave;
+  home.file.".config/fuzzel".source = ../../dotfiles/fuzzel;
+  home.file.".config/swaylock".source = ../../dotfiles/swaylock;
+  home.file.".config/hypr/".source = ../../dotfiles/hypr;
 }
 
 
--- hosts/shared/pkgs.nix
+++ hosts/shared/pkgs.nix
@@ -1,28 +1,30 @@
 { pkgs, ... }:
 
 {
-    environment.systemPackages = with pkgs; [
-        prefetch-npm-deps
-        nix-prefetch-git
-        chromium
-        discord
-        firefox-wayland
-        htop
-        jq
-        vlc
-        s3cmd
-        slack
-        discord
-        nodejs
-        bun
-        nvtop
-        obsidian
-        (callPackage ./programs/random-labels.nix {})
-        (callPackage ./programs/clever-tools.nix {})
-    ];
+  environment.systemPackages = with pkgs; [
+    prefetch-npm-deps
+    nix-prefetch-git
+    chromium
+    discord
+    firefox-wayland
+    htop
+    jq
+    vlc
+    s3cmd
+    slack
+    discord
+    nodejs
+    bun
+    nvtop
+    obsidian
+    tuxedo-rs
+    tailor-gui
+    (callPackage ./programs/random-labels.nix { })
+    (callPackage ./programs/clever-tools.nix { })
+  ];
 
-    fonts.packages = with pkgs; [
-        nerdfonts
-        font-awesome
-    ];
+  fonts.packages = with pkgs; [
+    nerdfonts
+    font-awesome
+  ];
 }
