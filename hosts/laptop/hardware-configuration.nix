fae1151b4133a513557d8aee28be3907606d4bc6
Florian Sanders <florian.sanders@clever-cloud.com>
2024-03-22 20:15:01 +0100

    feat(kernel): switch to latest

--- hosts/laptop/hardware-configuration.nix
+++ hosts/laptop/hardware-configuration.nix
@@ -5,30 +5,33 @@
 
 {
   imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+    [
+      (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/6fc89f7d-2a42-419b-94b2-5b5ef911f6c2";
+    {
+      device = "/dev/disk/by-uuid/6fc89f7d-2a42-419b-94b2-5b5ef911f6c2";
       fsType = "ext4";
     };
 
   boot.initrd.luks.devices."root".device = "/dev/disk/by-uuid/e125c490-0118-4bda-b078-e4d6c334b154";
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/41FF-2A42";
-      fsType = "vfat"; 
+    {
+      device = "/dev/disk/by-uuid/41FF-2A42";
+      fsType = "vfat";
       options = [ "umask=0077" "defaults" ];
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/ac4568f0-3f89-4ade-a012-a8ff3b24471e"; }
-    ];
+    [{ device = "/dev/disk/by-uuid/ac4568f0-3f89-4ade-a012-a8ff3b24471e"; }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
