13d195757133f827e5d1c5f53ce9efcc60d15dd4
Florian Sanders <florian.sanders@clever-cloud.com>
2024-03-30 10:19:58 +0100

    fix(hyprland): change WLR_DRM_DEVICES

--- flake.nix
+++ flake.nix
@@ -1,9 +1,5 @@
 {
   inputs = {
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-    # home-manager.url = "github:nix-community/home-manager/release-23.05";
-    # home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
