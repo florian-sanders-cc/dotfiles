0b6ba7b0db7b58c1592c1eec261a9d58443fb531
Florian Sanders <florian.sanders@clever-cloud.com>
2024-04-03 16:10:41 +0200

    fix(zsh): add missing aliases

--- hosts/shared/programs/zsh.nix
+++ hosts/shared/programs/zsh.nix
@@ -13,6 +13,10 @@
       upd-gnome = "sudo nix flake update '${config.home.homeDirectory}/.config/nixos-config/' && sudo nixos-rebuild switch --upgrade --flake '${config.home.homeDirectory}/.config/nixos-config#laptop-gnome'";
       upd-plasma = "sudo nix flake update '${config.home.homeDirectory}/.config/nixos-config/' && sudo nixos-rebuild switch --upgrade --flake '${config.home.homeDirectory}/.config/nixos-config#laptop-plasma'";
       upd-hypr = "sudo nix flake update '${config.home.homeDirectory}/.config/nixos-config' && sudo nixos-rebuild switch --upgrade --flake '${config.home.homeDirectory}/.config/nixos-config#laptop-hypr'";
+      nn = "cd ${config.home.homeDirectory}/Notes";
+      SN = "s3cmd -c ${config.home.homeDirectory}/s3cfgs/flo-clever.s3cfg sync --delete-removed ${config.home.homeDirectory}/Notes/ s3://flo-clever-notes";
+      SP = "s3cmd -c ${config.home.homeDirectory}/s3cfgs/flo-clever.s3cfg sync --delete-removed --exclude-from ${config.home.homeDirectory}/Projects/.s3ignore ${config.home.homeDirectory}/Projects s3://flo-projects-backup";
+      rt = "random-labels Hubert Mathieu Florian Bob Pierre Hélène --clipboard";
       clean = "sudo nix-collect-garbage -d && nix-collect-garbage";
       nxcfg = "cd ${config.home.homeDirectory}/.config/nixos-config";
       ww = "cd ${config.home.homeDirectory}/Projects/";
