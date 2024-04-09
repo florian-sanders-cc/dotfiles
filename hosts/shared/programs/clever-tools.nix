ba7009e2e5f1102ab6ae87d88506e1ff3befa96d
Florian Sanders <florian.sanders@clever-cloud.com>
2024-04-09 17:50:36 +0200

    chore(clever-tools): upgrade to 3.5.0

--- hosts/shared/programs/clever-tools.nix
+++ hosts/shared/programs/clever-tools.nix
@@ -5,15 +5,15 @@
 
 buildNpmPackage rec {
   pname = "clever-tools";
-  version = "3.4.0";
+  version = "3.5.0";
 
   src = fetchFromGitHub {
     owner = "CleverCloud";
     repo = "clever-tools";
     rev = "${version}";
-    hash = "sha256-3su+QU7TsgOghV35pqgLqzGlJNbSQ54IC2XxRh8RGkc=";
+    hash = "sha256-v60evwLnMxU5EGILAvCTtMsO0fWpjfc0TA7upHcrq9U=";
   };
 
-  npmDepsHash = "sha256-QZ1t0tA15ztQWnaYiajt4JdzzvqNxn1IjaVP/D9WyQk=";
+  npmDepsHash = "sha256-AK1CNtFTM3wgel5L2ekmXWHZhIerqKPd5i7X/kJm7No=";
   dontNpmBuild = true;
 }
