98d9b1bae168e625fbbefe5c805e073333394ef1
Florian Sanders <florian.sanders@clever-cloud.com>
2024-04-18 15:21:49 +0200

    feat(clever-tools): add completions

--- hosts/shared/programs/clever-tools.nix
+++ hosts/shared/programs/clever-tools.nix
@@ -1,19 +1,28 @@
 { fetchFromGitHub
 , buildNpmPackage
 , stdenv
+, pkgs
 }:
 
 buildNpmPackage rec {
   pname = "clever-tools";
-  version = "3.5.0";
+  version = "3.6.0";
+
+  nodejs = pkgs.nodejs-18_x;
 
   src = fetchFromGitHub {
     owner = "CleverCloud";
     repo = "clever-tools";
     rev = "${version}";
-    hash = "sha256-v60evwLnMxU5EGILAvCTtMsO0fWpjfc0TA7upHcrq9U=";
+    hash = "sha256-3LZ04YeVrKXa9rk3Wh4yS4zk4cUyT0fw7VnsaPXdKgs=";
   };
 
-  npmDepsHash = "sha256-AK1CNtFTM3wgel5L2ekmXWHZhIerqKPd5i7X/kJm7No=";
+  npmDepsHash = "sha256-dJpe+YgklzuwBF1FnTQxBCiIBF2gaG9MIp0cfRl+YfQ=";
   dontNpmBuild = true;
+
+  postInstall = ''
+    mkdir -p $out/share/{bash-completion/completions,zsh/site-functions}
+    $out/bin/clever --bash-autocomplete-script $out/bin/clever > $out/share/bash-completion/completions/clever
+    $out/bin/clever --zsh-autocomplete-script $out/bin/clever > $out/share/zsh/site-functions/_clever
+  '';
 }
