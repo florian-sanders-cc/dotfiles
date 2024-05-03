01b901956cb8f35bb69867660ee50816366cf128
Florian Sanders <florian.sanders@clever-cloud.com>
2024-05-03 13:53:38 +0200

    chore(dependencies): upgrade

--- dotfiles/nvim/lua/plugins/typescript-tools-lsp.lua
+++ dotfiles/nvim/lua/plugins/typescript-tools-lsp.lua
@@ -1,9 +1,24 @@
 -- vim.lsp.set_log_level("debug")
+if true then
+	return {}
+end
 return {
 	{
 		"pmizio/typescript-tools.nvim",
-		enabled = false,
-		opts = {},
+		enabled = true,
+		opts = {
+			tsserver = {
+				fallbackPath = string.gsub(
+					vim.fn.system("which tsserver"),
+					"/bin/tsserver",
+					"/lib/node_modules/typescript/lib/tsserver.js"
+				),
+			},
+			preferences = {
+				quotePreference = "single",
+				importModuleSpecifierEnding = "js",
+			},
+		},
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			{
