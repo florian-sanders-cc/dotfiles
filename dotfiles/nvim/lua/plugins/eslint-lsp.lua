7958a394b49eaaa55ad02e16eb43dbcb54dd0a9e
Florian Sanders <florian.sanders@clever-cloud.com>
2024-05-30 14:46:11 +0200

    chore(neovim/eslint-lsp): move custom-directory rules to local cfg

--- dotfiles/nvim/lua/plugins/eslint-lsp.lua
+++ dotfiles/nvim/lua/plugins/eslint-lsp.lua
@@ -1,33 +1,18 @@
 return {
-  "neovim/nvim-lspconfig",
-  ---@class PluginLspOpts
-  opts = {
-    ---@type lspconfig.options
-    servers = {
-      ---@type lspconfig.options.eslint
-      eslint = {
-        settings = {
-          options = {
-            rulePaths = {
-              "/home/flo/Projects/clever-components/eslint-rules",
-            },
-          },
-          workingDirectory = {
-            mode = "auto",
-          },
-        },
-      },
-    },
-    setup = {
-      eslint = function()
-        require("lazyvim.util").lsp.on_attach(function(client)
-          if client.name == "eslint" then
-            client.server_capabilities.documentFormattingProvider = true
-          elseif client.name == "typescript-tools" or client.name == "tsserver" then
-            client.server_capabilities.documentFormattingProvider = false
-          end
-        end)
-      end,
-    },
-  },
+	"neovim/nvim-lspconfig",
+	---@class PluginLspOpts
+	opts = {
+		---@type lspconfig.options
+		setup = {
+			eslint = function()
+				require("lazyvim.util").lsp.on_attach(function(client)
+					if client.name == "eslint" then
+						client.server_capabilities.documentFormattingProvider = true
+					elseif client.name == "typescript-tools" or client.name == "tsserver" then
+						client.server_capabilities.documentFormattingProvider = false
+					end
+				end)
+			end,
+		},
+	},
 }
