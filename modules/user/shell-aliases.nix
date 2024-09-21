5a94025371976fcfa5a5d56a4db1edd0c08c14d1
Florian Sanders <florian.sanders@clever-cloud.com>
2024-09-21 11:05:17 +0200

    fix(shell-aliases): cleanup & fix clean alias

--- modules/user/shell-aliases.nix
+++ modules/user/shell-aliases.nix
@@ -8,7 +8,6 @@ in
   # TODO: use function to generate update / rebuild aliases from user specs
   proAliases = {
     clever-dev = "${homeDirectory}/Projects/clever-tools/bin/clever.js";
-    clever-switch-account = "mv ${homeDirectory}/.config/clever-cloud/clever-tools.json ${homeDirectory}/.config/clever-cloud/clever-tools-tmp.json; mv ${homeDirectory}/.config/clever-cloud/clever-tools-alternative-account.json ${homeDirectory}/.config/clever-cloud/clever-tools.json; mv ${homeDirectory}/.config/clever-cloud/clever-tools-tmp.json ${homeDirectory}/.config/clever-cloud/clever-tools-alternative-account.json; clever profile";
     rt = "random-labels Hubert Mathieu Florian Bob Pierre Hélène --clipboard";
     SN = "s3cmd -c ${homeDirectory}/s3cfgs/flo-clever.s3cfg sync --delete-removed ${homeDirectory}/Notes/ s3://flo-clever-notes";
     SP = "s3cmd -c ${homeDirectory}/s3cfgs/flo-clever.s3cfg sync --delete-removed --exclude-from ${homeDirectory}/Projects/.s3ignore ${homeDirectory}/Projects s3://flo-projects-backup";
@@ -22,7 +21,7 @@ in
   commonAliases = {
     ll = "ls -l -a";
     nn = "cd ${homeDirectory}/Notes";
-    clean-nix = "sudo nix-collect-garbage -d; nix-collect-garbage";
+    clean = "sudo nix-collect-garbage -d; nix-collect-garbage";
     nxcfg = "cd ${homeDirectory}/.config/nixos-config";
     ww = "cd ${homeDirectory}/Projects/";
     setnode = "cat ${homeDirectory}/.config/nixos-config/node-shell/.envrc-example >> .envrc; direnv allow";
