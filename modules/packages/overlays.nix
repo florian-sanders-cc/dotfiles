2b0d44201820e0159bb5733e9688e550d6bbf775
Florian Sanders <florian.sanders@clever-cloud.com>
2024-09-21 11:07:49 +0200

    chore(overlays): switch back to prod `clever-tools`

--- modules/packages/overlays.nix
+++ modules/packages/overlays.nix
@@ -18,19 +18,23 @@
         pname = "clever-tools";
       clever-switch-profile = prev.callPackage ./clever-switch-profile.nix { };
         src = prev.fetchFromGitHub {
-          owner = "CleverCloud";
-          repo = "clever-tools";
-          rev = "02117066aa77ea2252eb270c4f10e2e140f98bc4";
-          hash = "sha256-90khKgGI6Oi0C9HOgaL2TNUoNoJLLsVJucjDB8RaINk=";
-        };
-
-        npmDepsHash = "sha256-s4dX6vU8t8LcXM5ksfRF5WHlNdVdgm5hUArFwfcArq4=";
-        npmDeps = final.fetchNpmDeps {
-          inherit src;
-          name = "${pname}-${version}-npm-deps";
-          hash = npmDepsHash;
-        };
-      });
+      # clever-tools = prev.clever-tools.overrideAttrs (_: rec {
+      #   pname = "clever-tools";
+      #   version = "3.8.3";
+      #   src = prev.fetchFromGitHub {
+      #     owner = "CleverCloud";
+      #     repo = "clever-tools";
+      #     rev = version;
+      #     hash = "sha256-70wyu8+Jb9kR5lIucBZG9UWIufMhsgMBMkT2ohGvE50=";
+      #   };
+      #
+      #   npmDepsHash = "sha256-LljwS6Rd/8WnGpxSHwCr87KWLaRR2i7sMdUuuprYiOE=";
+      #   npmDeps = final.fetchNpmDeps {
+      #     inherit src;
+      #     name = "${pname}-${version}-npm-deps";
+      #     hash = npmDepsHash;
+      #   };
+      # });
     })
   ];
 }
