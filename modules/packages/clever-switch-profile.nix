9fa2265a26ad41fe444673b5f6d4d8ac1e536c42
Florian Sanders <florian.sanders@clever-cloud.com>
2024-10-28 08:10:32 +0100

    refactor(packages/clever-switch-profile): improve `makeWrapper` usage

--- modules/packages/clever-switch-profile.nix
+++ modules/packages/clever-switch-profile.nix
@@ -1,7 +1,6 @@
 {
   stdenv,
   pkgs,
-  lib,
   ...
 }:
 
@@ -10,11 +9,11 @@ stdenv.mkDerivation {
   version = "0.1.0";
   src = ../../dotfiles/clever-tools;
   nativeBuildInputs = [ pkgs.makeWrapper ];
-  buildInputs = [ pkgs.nodejs-18_x ];
+
   installPhase = ''
-    mkdir -p $out/bin
-    cp clever-switch-profile.js $out/bin/clever-switch-profile
-    wrapProgram $out/bin/clever-switch-profile \
-      --prefix PATH : ${lib.makeBinPath [ pkgs.nodejs-18_x ]}
+    mkdir -p $out/bin $out/src
+    cp clever-switch-profile.js $out/src
+    makeWrapper ${pkgs.nodejs-18_x}/bin/node $out/bin/clever-switch-profile \
+      --add-flags "$out/src/clever-switch-profile.js"
   '';
 }
