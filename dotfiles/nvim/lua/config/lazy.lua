b469bbc6294638df916a07fd0b491a764f9b1f82
Florian Sanders <florian.sanders@clever-cloud.com>
2024-12-20 19:26:29 +0100

    fix(neovim/lazy): disable nix lang

--- dotfiles/nvim/lua/config/lazy.lua
+++ dotfiles/nvim/lua/config/lazy.lua
@@ -30,7 +30,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.lang.toml" },
 		{ import = "lazyvim.plugins.extras.lang.yaml" },
 		{ import = "lazyvim.plugins.extras.lang.git" },
-		{ import = "lazyvim.plugins.extras.lang.nix" },
+		-- { import = "lazyvim.plugins.extras.lang.nix" },
 		-- import/override with your plugins
 		{ import = "plugins" },
 	},
