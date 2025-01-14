7e5a038b7417752f302d1085540d9ab5612b26b7
Florian Sanders <florian.sanders@clever-cloud.com>
2025-01-14 19:21:07 +0100

    fix(packages/zed): add `nil` Language Server fox `nix` files

--- modules/packages/zed.nix
+++ modules/packages/zed.nix
@@ -21,6 +21,8 @@
     zed-editor
   ];
 
-  xdg.configFile."zed/settings.json".source = config.lib.file.mkOutOfStoreSymlink "${currentUser.homeDirectory}/.config/nixos-config/dotfiles/zed/settings.json";
-  xdg.configFile."zed/keymap.json".source = config.lib.file.mkOutOfStoreSymlink "${currentUser.homeDirectory}/.config/nixos-config/dotfiles/zed/keymap.json";
+  xdg.configFile."zed/settings.json".source =
+    config.lib.file.mkOutOfStoreSymlink "${currentUser.homeDirectory}/.config/nixos-config/dotfiles/zed/settings.json";
+  xdg.configFile."zed/keymap.json".source =
+    config.lib.file.mkOutOfStoreSymlink "${currentUser.homeDirectory}/.config/nixos-config/dotfiles/zed/keymap.json";
 }
