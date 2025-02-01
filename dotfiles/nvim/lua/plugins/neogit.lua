df308e787129c511c7392ce0304930f0ea9fd3c8
Florian Sanders <florian.sanders@clever-cloud.com>
2025-02-01 11:32:35 +0100

    feat(zed): set task to run neogit

--- dotfiles/nvim/lua/plugins/neogit.lua
+++ dotfiles/nvim/lua/plugins/neogit.lua
@@ -1,14 +1,12 @@
 return {
-  "NeogitOrg/neogit",
-  dependencies = {
-    "nvim-lua/plenary.nvim", -- required
-    "sindrets/diffview.nvim", -- optional - Diff integration
-
-    -- Only one of these is needed, not both.
-    "nvim-telescope/telescope.nvim", -- optional
-  },
-  keys = {
-    { "<leader>gn", "<cmd>Neogit<CR>", mode = { "n" }, desc = "Open current status and diff" },
-  },
-  config = true,
+	"NeogitOrg/neogit",
+	lazy = false,
+	dependencies = {
+		"nvim-lua/plenary.nvim", -- required
+		"sindrets/diffview.nvim", -- optional - Diff integration
+	},
+	keys = {
+		{ "<leader>gn", "<cmd>Neogit<CR>", mode = { "n" }, desc = "Open current status and diff" },
+	},
+	config = true,
 }
--- modules/packages/zed.nix
+++ modules/packages/zed.nix
@@ -14,4 +14,6 @@
     config.lib.file.mkOutOfStoreSymlink "${currentUser.homeDirectory}/.config/nixos-config/dotfiles/zed/settings.json";
   xdg.configFile."zed/keymap.json".source =
     config.lib.file.mkOutOfStoreSymlink "${currentUser.homeDirectory}/.config/nixos-config/dotfiles/zed/keymap.json";
+  xdg.configFile."zed/tasks.json".source =
+    config.lib.file.mkOutOfStoreSymlink "${currentUser.homeDirectory}/.config/nixos-config/dotfiles/zed/tasks.json";
 }
