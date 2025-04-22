0067f112c7f35f2bb522403315af3413bf5ca6d3
Florian Sanders <florian.sanders@clever-cloud.com>
2025-04-22 07:29:00 +0200

    chore(packages/zed-preveiw): bump to `0.183.8-pre`

--- modules/packages/zed-preview.nix
+++ modules/packages/zed-preview.nix
@@ -14,7 +14,7 @@
 }:
 
 let
-  version = "0.183.5-pre";
+  version = "0.183.8-pre";
 in
 stdenv.mkDerivation rec {
   pname = "zed-preview";
@@ -23,7 +23,7 @@ stdenv.mkDerivation rec {
   # Fetch the Linux binary tarball
   src = fetchurl {
     url = "https://github.com/zed-industries/zed/releases/download/v${version}/zed-linux-x86_64.tar.gz";
-    sha256 = "sha256-e45Sh7RkQHRE7WQDMf4n0nvcS9y272lEfoEZrJa0Ctw=";
+    sha256 = "sha256-cQ1o9IKkFvBWS9wJt0upUE/Br3wRWMF944vJTzc9Xuk=";
   };
 
   nativeBuildInputs = [
