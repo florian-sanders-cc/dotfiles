851a1962df032f07be106402e5c7b08aa2fcf496
Florian Sanders <florian.sanders@clever-cloud.com>
2025-06-17 08:26:29 +0200

    refactor(shell-aliases): replace --use-remote-sudo with --sudo

--- modules/user/shell-aliases.nix
+++ modules/user/shell-aliases.nix
@@ -11,18 +11,18 @@ in
     rt = "random-labels Hubert Mathieu Florian Bob Pierre Hélène Clara Marion --clipboard";
     SN = "s3cmd -c ${homeDirectory}/s3cfgs/flo-clever.s3cfg sync --delete-removed ${homeDirectory}/Notes/ s3://flo-clever-notes";
     SP = "s3cmd -c ${homeDirectory}/s3cfgs/flo-clever.s3cfg sync --delete-removed --exclude-from ${homeDirectory}/Projects/.s3ignore ${homeDirectory}/Projects s3://flo-projects-backup";
-    upd-pro = "nix flake update --flake '${nixosConfigPath}'; nixos-rebuild switch --upgrade --flake '${nixosConfigPath}#pro' --use-remote-sudo";
-    rebuild-pro = "nixos-rebuild switch --flake '${nixosConfigPath}#pro' --use-remote-sudo";
+    upd-pro = "nix flake update --flake '${nixosConfigPath}'; nixos-rebuild switch --upgrade --flake '${nixosConfigPath}#pro' --sudo";
+    rebuild-pro = "nixos-rebuild switch --flake '${nixosConfigPath}#pro' --sudo";
     ccprod = "clever-switch-profile user_e320e720-7c5c-4f7e-b9f0-0a598abe106c";
     ccdev = "clever-switch-profile user_e37814b0-954c-4e16-9a53-544b0f7cf58d";
   };
   persoAliases = {
-    upd-perso = "nix flake update --flake '${nixosConfigPath}'; nixos-rebuild switch --upgrade --flake '${nixosConfigPath}#perso' --use-remote-sudo";
-    rebuild-perso = "nixos-rebuild switch --flake '${nixosConfigPath}#perso' --use-remote-sudo";
+    upd-perso = "nix flake update --flake '${nixosConfigPath}'; nixos-rebuild switch --upgrade --flake '${nixosConfigPath}#perso' --sudo";
+    rebuild-perso = "nixos-rebuild switch --flake '${nixosConfigPath}#perso' --sudo";
   };
   persoWorkstationAliases = {
-    upd-perso-workstation = "nix flake update --flake '${nixosConfigPath}'; nixos-rebuild switch --upgrade --flake '${nixosConfigPath}#perso-workstation' --use-remote-sudo";
-    rebuild-perso-workstation = "nixos-rebuild switch --flake '${nixosConfigPath}#perso-workstation' --use-remote-sudo";
+    upd-perso-workstation = "nix flake update --flake '${nixosConfigPath}'; nixos-rebuild switch --upgrade --flake '${nixosConfigPath}#perso-workstation' --sudo";
+    rebuild-perso-workstation = "nixos-rebuild switch --flake '${nixosConfigPath}#perso-workstation' --sudo";
   };
   commonAliases = {
     ll = "ls -l -a";
