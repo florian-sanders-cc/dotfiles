3bf72273553afbb0be579f9a03fc35ac1dabf96c
Florian Sanders <florian.sanders@clever-cloud.com>
2025-08-01 21:57:56 +0200

    feat(neovim): disable `css-variables-lsp`

--- dotfiles/nvim/lua/plugins/css-variables-lsp.lua
+++ dotfiles/nvim/lua/plugins/css-variables-lsp.lua
@@ -1,31 +1,32 @@
-return {
-	"neovim/nvim-lspconfig",
-	---@class PluginLspOpts
-	opts = {
-		setup = {
-			css_variables = function(_, opts)
-				opts.filetypes =
-					{ "typescript", "javascript", "javascriptreact", "typescriptreact", "css", "scss", "less" }
-			end,
-		},
-		servers = {
-			css_variables = {
-				blacklistFolders = {
-					"**/.cache",
-					"**/.DS_Store",
-					"**/.git",
-					"**/.hg",
-					"**/.next",
-					"**/.svn",
-					"**/bower_components",
-					"**/CVS",
-					"**/dist",
-					"**/node_modules",
-					"**/tests",
-					"**/tmp",
-				},
-				lookupFiles = { "**/*.less", "**/*.scss", "**/*.sass", "**/*.css" },
-			},
-		},
-	},
-}
+return {}
+-- return {
+-- 	"neovim/nvim-lspconfig",
+-- 	---@class PluginLspOpts
+-- 	opts = {
+-- 		setup = {
+-- 			css_variables = function(_, opts)
+-- 				opts.filetypes =
+-- 					{ "typescript", "javascript", "javascriptreact", "typescriptreact", "css", "scss", "less" }
+-- 			end,
+-- 		},
+-- 		servers = {
+-- 			css_variables = {
+-- 				blacklistFolders = {
+-- 					"**/.cache",
+-- 					"**/.DS_Store",
+-- 					"**/.git",
+-- 					"**/.hg",
+-- 					"**/.next",
+-- 					"**/.svn",
+-- 					"**/bower_components",
+-- 					"**/CVS",
+-- 					"**/dist",
+-- 					"**/node_modules",
+-- 					"**/tests",
+-- 					"**/tmp",
+-- 				},
+-- 				lookupFiles = { "**/*.less", "**/*.scss", "**/*.sass", "**/*.css" },
+-- 			},
+-- 		},
+-- 	},
+-- }
