6916874839b30330c1a93960dc662ae88a816c56
Florian Sanders <florian.sanders@clever-cloud.com>
2025-09-21 15:18:03 +0200

    feat(packages/kitty): enable

---
+++ modules/packages/kitty.nix
@@ -0,0 +1,42 @@
+{ ... }:
+
+{
+  programs.kitty = {
+    enable = true;
+    settings = {
+      font_size = 14.0;
+      font_family = "JetBrainsMono NFM Thin";
+      bold_font = "JetBrainsMono NFM Light";
+      italic_font = "JetBrainsMono NFM Thin Italic";
+      bold_italic_font = "JetBrainsMono NFM Light Italic";
+      tab_bar_style = "hidden";
+      confirm_os_window_close = 0;
+      modify_font = "cell_height 120%";
+      background = "#2e3440";
+      foreground = "#d8dee9";
+      selection_background = "#eceff4";
+      selection_foreground = "#4c566a";
+      url_color = "#8fbcbb";
+      cursor = "#eceff4";
+      active_border_color = "#88c0d0";
+      inactive_border_color = "#4c566a";
+      bell_border_color = "#b48ead";
+      color0 = "#3b4252";
+      color1 = "#bf616a";
+      color2 = "#a3be8c";
+      color3 = "#ebcb8b";
+      color4 = "#81a1c1";
+      color5 = "#b48ead";
+      color6 = "#88c0d0";
+      color7 = "#e5e9f0";
+      color8 = "#4c566a";
+      color9 = "#bf616a";
+      color10 = "#a3be8c";
+      color11 = "#ebcb8b";
+      color12 = "#81a1c1";
+      color13 = "#b48ead";
+      color14 = "#8fbcbb";
+      color15 = "#eceff4";
+    };
+  };
+}
