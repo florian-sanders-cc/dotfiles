7e5ea30ddcd6f356f41ff9739ada921559fa3ce5
Florian Sanders <florian.sanders@clever-cloud.com>
2025-10-26 10:27:14 +0100

    chore(wc-ls): upgrade web-components-language-server to 0.0.20
    Update wc-language-server from 0.0.8 to 0.0.20 with improved
    hash and minor script cleanup.

--- modules/packages/wc-ls.nix
+++ modules/packages/wc-ls.nix
@@ -1,18 +1,18 @@
-{ stdenv
-, fetchurl
-, lib
-, nodejs
-, unzip
-,
+{
+  stdenv,
+  fetchurl,
+  lib,
+  nodejs,
+  unzip,
 }:
 
 stdenv.mkDerivation (finalAttrs: rec {
   pname = "wc-language-server";
-  version = "0.0.8";
+  version = "0.0.20";
 
   src = fetchurl {
     url = "https://open-vsx.org/api/wc-toolkit/web-components-language-server/${version}/file/wc-toolkit.web-components-language-server-${version}.vsix";
-    hash = "sha256-2YNY+RZTki+Z12AiGtXPqn3g5vMs7s44yjdGtdsSiWM=";
+    hash = "sha256-RGpTtxJze9IdU9s4I5kbd7J2yIMWm41C5+PtkHcgvkU=";
   };
 
   nativeBuildInputs = [
@@ -28,22 +28,21 @@ stdenv.mkDerivation (finalAttrs: rec {
 
   installPhase = ''
         runHook preInstall
-    
+
         mkdir -p $out/bin
         mkdir -p $out/lib/wc-language-server
-    
+
         # Extract and copy the extension contents
         cp -r extension/. $out/lib/wc-language-server/
-    
+
         # Create wrapper script that runs server.js from dist directory
         cat > $out/bin/wc-language-server << EOF
     #!/bin/sh
-    cd $out/lib/wc-language-server
-    exec ${nodejs}/bin/node dist/server.js "\$@"
+    exec ${nodejs}/bin/node $out/lib/wc-language-server/dist/server.js "\$@"
     EOF
-    
+
         chmod +x $out/bin/wc-language-server
-    
+
         runHook postInstall
   '';
 
@@ -55,4 +54,3 @@ stdenv.mkDerivation (finalAttrs: rec {
     maintainers = [ ];
   };
 })
-
