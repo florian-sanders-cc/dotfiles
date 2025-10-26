2963fd6adf10e164c0a197bc880d04f8b70a5086
Florian Sanders <florian.sanders@clever-cloud.com>
2025-10-26 10:21:13 +0100

    feat(desktop): remove Hyprland desktop environment
    Remove all Hyprland-related configuration files and module definitions.
    This desktop environment is no longer in use.

--- config-specifications.nix
+++ config-specifications.nix
@@ -6,25 +6,30 @@
       email = "florian.sanders@clever-cloud.com";
       signingKey = "684168075E28E749";
       homeDirectory = "/home/flo-pro";
+      gpgAuthKeygrip = "DA297557775E21E76FCBCE6C24295BE213D3BD1C";
+      gpgSignKeygrip = "E6223B67A87A3156E73B88A604F079489275E3D3";
     };
     perso = {
       name = "flo-perso";
       email = "sanders.florian@gmail.com";
       signingKey = "32343690B0FD8FBE";
       homeDirectory = "/home/flo-perso";
+      gpgAuthKeygrip = "204ED92B49B381141FB96E8A51BF930F62214CB9";
+      gpgSignKeygrip = "4109515F1927FE595547F3CCEDC8764253C92397";
     };
     perso-workstation = {
       name = "flo-perso-workstation";
       email = "sanders.florian@gmail.com";
       signingKey = "F26A9005C2DC3FB7";
       homeDirectory = "/home/flo-perso-workstation";
+      gpgAuthKeygrip = "6EDF2C2580763C5447F637AFBE930E626149530A";
+      gpgSignKeygrip = "122D9EE3BCD9EBA2ABC322B5C93D5DC4D463493D";
     };
   };
 
   desktops = {
     plasma = "plasma";
     gnome = "gnome";
-    hyprland = "hyprland";
     cosmic = "cosmic";
     niri = "niri";
     cosmic-niri = "cosmic-niri";
--- modules/desktop-env/default.nix
+++ modules/desktop-env/default.nix
@@ -8,7 +8,6 @@ in
   imports = lib.flatten [
     (lib.optional (currentDesktop == configSpecs.desktops.plasma) ./kde-plasma.nix)
     (lib.optional (currentDesktop == configSpecs.desktops.gnome) ./gnome.nix)
-    (lib.optional (currentDesktop == configSpecs.desktops.hyprland) ./hyprland.nix)
     (lib.optional (currentDesktop == configSpecs.desktops.cosmic) ./cosmic.nix)
     (lib.optional (currentDesktop == configSpecs.desktops.niri) ./niri.nix)
     (lib.optional (currentDesktop == configSpecs.desktops.cosmic-niri) ./cosmic-niri.nix)
