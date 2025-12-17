4ad8426af34c69783d4673345f8fddc2fca655c5
Florian Sanders <florian.sanders@clever-cloud.com>
2025-12-17 08:30:30 +0100

    chore(desktop-env/niri): bump `noctalia-shell` to `3.7.0`

--- flake.nix
+++ flake.nix
@@ -51,7 +51,7 @@
     };
 
     noctalia = {
-      url = "github:noctalia-dev/noctalia-shell?ref=v3.6.2";
+      url = "github:noctalia-dev/noctalia-shell?ref=v3.7.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
