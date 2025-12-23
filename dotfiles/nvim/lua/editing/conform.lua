4484ca8ec8b08e410d9b7b97fa881f8b25c0b6ab
Florian Sanders <florian.sanders@clever-cloud.com>
2025-12-23 23:25:19 +0100

    refactor(packages/neovim): remove prettierd from formatters

--- dotfiles/nvim/lua/editing/conform.lua
+++ dotfiles/nvim/lua/editing/conform.lua
@@ -16,10 +16,10 @@ require("conform").setup({
   formatters_by_ft = {
     lua = { "stylua" },
     nix = { "nixfmt" },
-    javascript = { "prettierd", "prettier", stop_after_first = true },
-    typescript = { "prettierd", "prettier", stop_after_first = true },
-    json = { "prettierd", "prettier", stop_after_first = true },
-    jsonc = { "prettierd", "prettier", stop_after_first = true },
+    javascript = { "prettier", stop_after_first = true },
+    typescript = { "prettier", stop_after_first = true },
+    json = { "prettier", stop_after_first = true },
+    jsonc = { "prettier", stop_after_first = true },
   },
   formatters = {
     stylua = {
