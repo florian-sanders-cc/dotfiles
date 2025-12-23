953db9fede66c40e8829d987c068ef279fa83093
Florian Sanders <florian.sanders@clever-cloud.com>
2025-12-23 23:26:41 +0100

    feat(user/shell-aliases): replace `sudo` with `run0`

--- modules/user/shell-aliases.nix
+++ modules/user/shell-aliases.nix
@@ -11,23 +11,23 @@ in
     rt = "random-labels Hubert Mathieu Florian Bob Pierre Hélène Clara Marion --clipboard";
     SN = "s3cmd -c ${homeDirectory}/s3cfgs/flo-clever.s3cfg sync --delete-removed ${homeDirectory}/Notes/ s3://flo-clever-notes";
     SP = "s3cmd -c ${homeDirectory}/s3cfgs/flo-clever.s3cfg sync --delete-removed --exclude-from ${homeDirectory}/Projects/.s3ignore ${homeDirectory}/Projects s3://flo-projects-backup";
-    upd-pro = "nix flake update --flake '${nixosConfigPath}'; nixos-rebuild switch --upgrade --flake '${nixosConfigPath}#pro' --sudo";
-    rebuild-pro = "nixos-rebuild switch --flake '${nixosConfigPath}#pro' --sudo";
+    upd-pro = "nix flake update --flake '${nixosConfigPath}'; run0 nixos-rebuild switch --upgrade --flake '${nixosConfigPath}#pro'";
+    rebuild-pro = "run0 nixos-rebuild switch --flake '${nixosConfigPath}#pro'";
     ccprod = "clever-switch-profile user_e320e720-7c5c-4f7e-b9f0-0a598abe106c";
     ccdev = "clever-switch-profile user_e37814b0-954c-4e16-9a53-544b0f7cf58d";
   };
   persoAliases = {
-    upd-perso = "nix flake update --flake '${nixosConfigPath}'; nixos-rebuild switch --upgrade --flake '${nixosConfigPath}#perso' --sudo";
-    rebuild-perso = "nixos-rebuild switch --flake '${nixosConfigPath}#perso' --sudo";
+    upd-perso = "nix flake update --flake '${nixosConfigPath}'; run0 nixos-rebuild switch --upgrade --flake '${nixosConfigPath}#perso'";
+    rebuild-perso = "run0 nixos-rebuild switch --flake '${nixosConfigPath}#perso'";
   };
   persoWorkstationAliases = {
-    upd-perso-workstation = "nix flake update --flake '${nixosConfigPath}'; nixos-rebuild switch --upgrade --flake '${nixosConfigPath}#perso-workstation' --sudo";
-    rebuild-perso-workstation = "nixos-rebuild switch --flake '${nixosConfigPath}#perso-workstation' --sudo";
+    upd-perso-workstation = "nix flake update --flake '${nixosConfigPath}'; run0 nixos-rebuild switch --upgrade --flake '${nixosConfigPath}#perso-workstation'";
+    rebuild-perso-workstation = "run0 nixos-rebuild switch --flake '${nixosConfigPath}#perso-workstation'";
   };
   commonAliases = {
     ll = "ls -l -a";
     nn = "cd ${homeDirectory}/Notes";
-    clean = "sudo nix-collect-garbage --delete-older-than 7d; nix-collect-garbage --delete-older-than 7d";
+    clean = "run0 nix-collect-garbage --delete-older-than 7d; nix-collect-garbage --delete-older-than 7d";
     nxcfg = "cd ${homeDirectory}/.config/nixos-config";
     ww = "cd ${homeDirectory}/Projects/";
     setnode = "cat ${homeDirectory}/.config/nixos-config/node-shell/.envrc-example >> .envrc; direnv allow";
