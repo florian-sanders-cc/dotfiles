1fb87a9a1ceb0e4844208a1fc900fee6db9ee269
Florian Sanders <florian.sanders@clever-cloud.com>
2026-03-24 10:12:58 +0100

    noctalia-shell: bump to `4.7.1`

--- flake.nix
+++ flake.nix
@@ -51,7 +51,7 @@
     };
 
     noctalia = {
-      url = "github:noctalia-dev/noctalia-shell?ref=v4.6.7";
+      url = "github:noctalia-dev/noctalia-shell?ref=v4.7.1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
