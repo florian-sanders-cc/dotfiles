dbd4a82aec8ffc8a78f4020e3960e57a849dc424
Florian Sanders <florian.sanders@clever-cloud.com>
2026-04-24 15:26:46 +0200

    shell-aliases: replace `switch` with `boot`

--- modules/user/shell-aliases.nix
+++ modules/user/shell-aliases.nix
@@ -11,18 +11,18 @@ in
     rt = "random-labels Hubert Mathieu Florian Bob Pierre Hélène Clara Marion --clipboard";
     SN = "s3cmd -c ${homeDirectory}/s3cfgs/flo-clever.s3cfg sync --delete-removed ${homeDirectory}/Notes/ s3://flo-clever-notes";
     SP = "s3cmd -c ${homeDirectory}/s3cfgs/flo-clever.s3cfg sync --delete-removed --exclude-from ${homeDirectory}/Projects/.s3ignore ${homeDirectory}/Projects s3://flo-projects-backup";
-    upd-pro = "nix flake update --flake '${nixosConfigPath}'; run0 nixos-rebuild switch --flake '${nixosConfigPath}#pro'";
-    rebuild-pro = "run0 nixos-rebuild switch --flake '${nixosConfigPath}#pro'";
+    upd-pro = "nix flake update --flake '${nixosConfigPath}'; run0 nixos-rebuild boot --flake '${nixosConfigPath}#pro'";
+    rebuild-pro = "run0 nixos-rebuild boot --flake '${nixosConfigPath}#pro'";
     ccprod = "clever profile switch -a prod";
     ccdev = "clever profile switch -a dev";
   };
   persoAliases = {
-    upd-perso = "nix flake update --flake '${nixosConfigPath}'; run0 nixos-rebuild switch --flake '${nixosConfigPath}#perso'";
-    rebuild-perso = "run0 nixos-rebuild switch --flake '${nixosConfigPath}#perso'";
+    upd-perso = "nix flake update --flake '${nixosConfigPath}'; run0 nixos-rebuild boot --flake '${nixosConfigPath}#perso'";
+    rebuild-perso = "run0 nixos-rebuild boot --flake '${nixosConfigPath}#perso'";
   };
   persoWorkstationAliases = {
-    upd-perso-workstation = "nix flake update --flake '${nixosConfigPath}'; run0 nixos-rebuild switch --flake '${nixosConfigPath}#perso-workstation'";
-    rebuild-perso-workstation = "run0 nixos-rebuild switch --flake '${nixosConfigPath}#perso-workstation'";
+    upd-perso-workstation = "nix flake update --flake '${nixosConfigPath}'; run0 nixos-rebuild boot --flake '${nixosConfigPath}#perso-workstation'";
+    rebuild-perso-workstation = "run0 nixos-rebuild boot --flake '${nixosConfigPath}#perso-workstation'";
   };
   commonAliases = {
     ll = "ls -l -a";
